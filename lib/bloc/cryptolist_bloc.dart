5d70795db1aa8f6d8a8b75e8a54240d1770c3295
Amirreza <imamirreza.kh@gmail.com>
2023-07-22 01:19:07 +0330

    final commit

--- lib/bloc/cryptolist_bloc.dart
+++ lib/bloc/cryptolist_bloc.dart
@@ -1,8 +1,8 @@
-import 'package:bloc/bloc.dart';
 import 'package:coin_watch/bloc/cryptolist_event.dart';
 import 'package:coin_watch/bloc/cryptolist_state.dart';
 import 'package:coin_watch/data/repository/crypto_list_repository.dart';
 import 'package:coin_watch/di/di.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CryptoListBloc extends Bloc<CryptoListEvent, CryptoListState> {
   final ICryptoListRepository _repository = locator.get();
